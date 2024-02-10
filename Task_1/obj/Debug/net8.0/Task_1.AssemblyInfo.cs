813eac9399cb6a8090d6f072e15069b01ef550d2
Михаил Лубенцов <wispergently061@gmail.com>
2024-02-10 23:33:05 +0500

    Решение к ДЗ, где проверяем число на кратность на 7 и 23

--- Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
+++ Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8870da305d50803b72b76f7e06e03261665a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
